a0bdefc42432cda177f7bd67d5968043aa6797a6
Anders Skarby <anders@skarby.info>
2025-02-10 17:11:19 +0100

    Attempting to make apply.sh script work from any directory

--- bin/mac/apply.sh
+++ bin/mac/apply.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-SCRIPT_DIR =$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-ansible-playbook -i "localhost," -c local "$BASH_SOURCE/../../ansible_dev_mac.yml" --ask-become-pass
+SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+ansible-playbook -i "localhost," -c local "$SCRIPT_DIR/../../ansible_dev_mac.yml" --ask-become-pass
