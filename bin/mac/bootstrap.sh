452576213f613ed333c5b4c4af4c95a4bac8f1a8
Anders Skarby <anders@skarby.info>
2025-02-10 16:19:52 +0100

    Added step to clone repository, and hopefully install requirements into Ansible-galasxy

--- bin/mac/bootstrap.sh
+++ bin/mac/bootstrap.sh
@@ -8,7 +8,7 @@ echo #
 xcode-select -p  > /dev/null 2> /dev/null
 
 if [ $? -ne 0 ]; then
-    echo "[INFO] Xcode doesn't seem to have been installed - launching installer"
+    echo "[INFO] Xcode (command line developer tools) doesn't seem to have been installed - launching installer"
     echo ""
     echo "... please look for Xcode installation window (should be visible on screen, or at least in Dock)"
     echo "    when installation is done, run this command again!"
@@ -20,6 +20,19 @@ else
     echo "[INFO] Xcode seems to be installed, moving on to next step!"
 fi
 
+####################
+# Clone repository #
+####################
+DEV_SETUP_HOME="$HOME/.ansible-dev-setup"
+if [ -d "$DEV_SETUP_HOME/.git" ]; then
+  echo "[INFO] Github repository has been cloned, updating it instead!"
+  git --git-dir="$DEV_SETUP_HOME/.git" pull origin main
+
+else 
+  echo "[INFO] Github repository has NOT been cloned, cloning into: $DEV_SETUP_HOME..."
+  git clone https://github.com/askarby/ansible-dev-setup.git $DEV_SETUP_HOME
+fi
+
 ###################
 # Install Ansible #
 ###################
@@ -27,6 +40,9 @@ pip3 list | grep ansible
 if [ $? -ne 0 ]; then
     echo "[INFO] Ansible doesn't seem to have been installed - installing!"
     echo ""
+    echo "... you'll be prompted to enter your "
+    echo ""
+
     sudo pip3 install --ignore-installed ansible
 else 
     echo "[INFO] Ansible seems to be installed, moving on to next step!"
@@ -35,4 +51,9 @@ fi
 ################################
 # Install Ansible requirements #
 ################################
-ansible-galaxy install -r requirements.yml
+echo "[INFO] Adding requirements to Ansible Galaxy!"
+ansible-galaxy install -r "$DEV_SETUP_HOME/requirements.yml"
+
+echo "[INFO] Bootstrap completed!"
+echo ""
+echo "... you're now ready to install ansible packages, go ahead and invoke /bin/mac/apply.sh!"
