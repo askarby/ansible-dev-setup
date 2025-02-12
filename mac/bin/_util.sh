7e99342495a3225f645aa7f31d528546d9746354
Anders Skarby <anders@skarby.info>
2025-02-12 13:52:25 +0100

    Fix to utils

--- mac/bin/_util.sh
+++ mac/bin/_util.sh
@@ -9,8 +9,8 @@ fi
 
 assert_directory_exists () {
     local DIRECTORY="$1"
-    local MESSAGE="$2" || "[ERROR] Directory '$DIRECTORY' doesn't exist"
-    local EXIT_CODE="$3" || -1
+    local MESSAGE"${2:="[ERROR] Directory '$DIRECTORY' doesn't exist"}"
+    local EXIT_CODE="${3:=-1}"
 
     if [ ! -d "${DIRECTORY}" ]; then
         echo MESSAGE
