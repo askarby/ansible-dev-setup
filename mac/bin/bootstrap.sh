cd065f215c7c84123c7647004b25269059cd1e91
Anders Skarby <anders@skarby.info>
2025-02-12 13:48:21 +0100

    Fixed error in bootstrap

--- mac/bin/bootstrap.sh
+++ mac/bin/bootstrap.sh
@@ -73,7 +73,7 @@ if [ -d "$DOTFILES_STOW" ]; then
 else 
   echo "[INFO] Github repository (with dot files) has NOT been cloned, cloning into: $DEV_SETUP_HOME..."
   echo ""
-  git clone REPO_DOTFILES $DEV_SETUP_HOME
+  git clone $REPO_DOTFILES $DEV_SETUP_HOME
   echo ""
 fi
 
